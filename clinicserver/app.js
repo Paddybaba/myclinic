5baaa5bb5849ff4d6d2d8d5c5c31403eb51ab15f
unknown <paddybaba@gmail.com>
2021-09-30 19:06:36 +0530

    Successfully working database

--- clinicserver/app.js
+++ clinicserver/app.js
@@ -2,12 +2,11 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const express = require("express");
 dotenv.config({ path: "./config.env" });
-
+const app = express();
 require("./database/conn");
 // import Patient model from database patientSchema
 const Patient = require("./database/models/patientSchema");
-
-const app = express();
+app.use(express.json());
 
 app.use(require("./router/paths"));
 
--- clinicserver/router/paths.js
+++ clinicserver/router/paths.js
@@ -1,12 +1,52 @@
 const express = require("express");
 const router = express.Router();
+const Patient = require("../database/models/patientSchema");
 
 router.get("/", (req, res) => {
   res.send("Hello from from my server router");
 });
 
-router.post("/patientregistration", (req, res) => {
-  console.log(req.body);
-  res.send("data received");
+// REGISTERING PATIENTS /////////////////
+router.post("/patientregistration", async (req, res) => {
+  const {
+    patient_name,
+    patient_addrress,
+    patient_age,
+    patient_gender,
+    patient_fee,
+    patient_visit,
+    patient_phone,
+    consulting_doctor,
+  } = req.body;
+
+  if (!patient_name || !patient_visit || !consulting_doctor) {
+    res.status(422).json({ error: "Please fill the required fields !!" });
+    return;
+  }
+
+  const patient = new Patient({
+    patient_name,
+    patient_addrress,
+    patient_age,
+    patient_gender,
+    patient_fee,
+    patient_visit,
+    patient_phone,
+    consulting_doctor,
+  });
+
+  const saved = patient.save();
+  try {
+    if (saved) {
+      res.status(200).json({ message: "Patient details saved succesfully" });
+    } else {
+      res
+        .status(500)
+        .json({ error: "Error while genrating patient record !!" });
+    }
+  } catch {
+    res.json({ error: "Some error occurred" });
+  }
 });
+
 module.exports = router;
