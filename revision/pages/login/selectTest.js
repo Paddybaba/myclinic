f4abf8e0bc0eb7618687b800af9f0ba67312d553
unknown <paddybaba@gmail.com>
2021-10-31 23:28:30 +0530

    select options page1

--- revision/pages/login/selectTest.js
+++ revision/pages/login/selectTest.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const selectTest = () => {
+const selectTest = (props) => {
+  //   console.log(props);
   return (
     <>
       <div className="container-fluid">
         <div className="row">
           <div className="col-10 mx-auto">
             <div>
+              <h3>Welcome : {props.student.student}</h3>
               <h3>Select your test paper</h3>
             </div>
           </div>
@@ -16,4 +19,8 @@ const selectTest = () => {
   );
 };
 
-export default selectTest;
+const mdtp = (dispatch) => ({});
+const mstp = (state) => ({
+  student: state.studentReducer.user,
+});
+export default connect(mstp, mdtp)(selectTest);
